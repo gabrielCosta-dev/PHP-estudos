65c3dd3863d02631eac8eeba2c5ce24ba1ad74d1
Gabriel Lima <lima20gabriel@gmail.com>
2023-11-23 15:34:58 -0300

    Create if_else.php

---
+++ if_else.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+        //== Semelhante
+        //=== Idêntico
+        //!= OU <> Diferente
+        //!== Não Idêntico
+        //> Maior que
+        //< Menor que
+        //>= Maior ou igual que
+        //<= Menor ou igual que
+
+        if (1 >= '2') echo 'Verdadeiro'; else echo 'Falso';
+        
+        
+     ?>
+</body>
+</html>
