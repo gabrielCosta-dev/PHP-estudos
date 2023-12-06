d252cb7904b5cee550b659718a550e70b64b2f31
Gabriel Lima <lima20gabriel@gmail.com>
2023-12-06 17:29:00 -0300

    Create operadores_aritmeticos.php

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php 
+        /* $num1 = 13; $num2 = 4;
+
+        echo "A soma entre $num1 e $num2 é: " .$num1+$num2. '.';
+        echo '<br>';
+        echo "A subtração entre $num1 e $num2 é: " .$num1-$num2. '.';
+        echo '<br>';
+        echo "A multiplicação entre $num1 e $num2 é: " .$num1*$num2. '.';
+        echo '<br>';
+        echo "A divisão entre $num1 e $num2 é: " .$num1/$num2. '.';
+        echo '<br>';
+        echo "O módulo entre $num1 e $num2 é: " .$num1%$num2. '.'; */
+
+        $num = 10;
+
+        $num += 10;
+
+        echo $num;
+    ?>
+</body>
+</html>
