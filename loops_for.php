ebde4ea3afa4dab0892766232c8384ed0f00aa71
Gabriel Lima <lima20gabriel@gmail.com>
2024-02-01 15:02:04 -0300

    Create loops_for.php

---
+++ loops_for.php
@@ -0,0 +1,16 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php 
+        
+        for ($i = 0; $i < 10; $i++) { 
+            echo "$i ";
+        }
+        
+    ?>
+</body>
+</html>
